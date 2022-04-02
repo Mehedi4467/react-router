674118be9cec01457336b7ddd9ab9c2d2756c02e
Mehedi4467 <mehedihassan4467@gmail.comm>
2022-04-02 22:09:08 +0600

    not found route create

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from './Componenets/About/About';
 import Contact from './Componenets/Contact/Contact';
 import Header from './Componenets/Header/Header';
 import Home from './Componenets/Home/Home';
+import NotFound from './Componenets/NotFound/NotFound';
 import Shop from './Componenets/Shop/Shop';
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <Route path='/shop' element={<Shop></Shop>}></Route>
         <Route path='/about' element={<About></About>}></Route>
         <Route path='/contact' element={<Contact></Contact>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
     </div>
   );
