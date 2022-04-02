e03d8b2de55ac78bba9e037f514a88e99e460356
Mehedi4467 <mehedihassan4467@gmail.comm>
2022-04-03 02:45:26 +0600

    spinner added

--- src/Componenets/ProductDatail/ProductDatail.js
+++ src/Componenets/ProductDatail/ProductDatail.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const ProductDatail = () => {
     const { productId } = useParams();
+    const [loading, setLoading] = useState(true);
+    const [product, setProduct] = useState([]);
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/photos/${productId}`)
+            .then(res => res.json())
+            .then(data => setProduct(data));
+        setLoading(false);
+    }, [productId]);
     return (
         <div>
-            <h1>Product Details : {productId}</h1>
+            <img src={product.thumbnailUrl} alt="" />
+            <p>{product.title}-{product.id}</p>
+
+            <div className={` ${loading ? 'flex justify-center mt-10' : 'hidden'}`}>
+                <svg class="animate-spin bg-black  h-5 w-5 mr-3 ..." viewBox="0 0 24 24">
+
+                </svg>
+            </div>
+
         </div>
     );
 };
